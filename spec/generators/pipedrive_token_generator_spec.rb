ece5f0401bd55afa480b8a0fdeba987e0c0447cc
DiodonHystrix <diodonhystrix7@gmail.com>
2018-06-06 16:35:36 +0200

    Replace <<- with <<~

--- spec/generators/pipedrive_token_generator_spec.rb
+++ spec/generators/pipedrive_token_generator_spec.rb
@@ -15,26 +15,26 @@ RSpec.describe Pipedrive::Generators::PipedriveTokenGenerator, type: :generator 
 
   it "creates a model file" do
     assert_file "app/models/pipedrive_token.rb",
-    <<-CONTENT
-class PipedriveToken < ApplicationRecord
-end
+    <<~CONTENT
+      class PipedriveToken < ApplicationRecord
+      end
     CONTENT
   end
 
   it 'creates a miration file' do
     assert_migration "db/migrate/create_pipedrive_tokens.rb",
-    <<-CONTENT
-# frozen_string_literal: true
+    <<~CONTENT
+      # frozen_string_literal: true
 
-class CreatePipedriveTokens < ActiveRecord::Migration[5.2]
-  def change
-    create_table :pipedrive_tokens do |t|
-      t.string :access_token
-      t.string :refresh_token
-      t.datetime :expires_at
-    end
-  end
-end
+      class CreatePipedriveTokens < ActiveRecord::Migration[5.2]
+        def change
+          create_table :pipedrive_tokens do |t|
+            t.string :access_token
+            t.string :refresh_token
+            t.datetime :expires_at
+          end
+        end
+      end
     CONTENT
   end
 end
