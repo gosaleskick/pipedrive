63cf152013eacb5ff2e6710312f2cffbec2949e5
DiodonHystrix <diodonhystrix7@gmail.com>
2018-06-11 12:22:56 +0200

    Add new column to migration generator

--- spec/generators/pipedrive_token_generator_spec.rb
+++ spec/generators/pipedrive_token_generator_spec.rb
@@ -32,9 +32,12 @@ RSpec.describe Pipedrive::Generators::PipedriveTokenGenerator, type: :generator 
             t.string :access_token
             t.string :refresh_token
             t.datetime :expires_at
+            t.string :company_domain, null: false
 
             t.timestamps null: false
           end
+
+          add_index :pipedrive_tokens, :company_domain
         end
       end
     CONTENT
