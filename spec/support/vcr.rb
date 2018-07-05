31d352e696f51384c68267b0097af701bbb12dc6
DiodonHystrix <diodonhystrix7@gmail.com>
2018-07-05 14:33:01 +0200

    More strict VCR matching

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 VCR.configure do |c|
+  c.default_cassette_options = { match_requests_on: [:method, :uri, :body] }
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :typhoeus
   c.configure_rspec_metadata!
